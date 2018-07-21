805a1c0f6cb791d0467ee57f52e9c3a5c1b6cc1a
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-07-21 21:39:03 +0200

    First commit!

---
+++ Aura Debugger/Aura Debugger/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Aura_Debugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(701, 369);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Waiting at XXX.XXX.X.XX, 4224";
+            this.label1.Visible = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(726, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Aura Debugger v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Aura Debugger/Aura Debugger/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          Aura's Debugger!
+* PROGRAMMERS:      Valentin Charbonnier <valentinbreiz@gmail.com>
+*/
+
+using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Aura_Debugger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string IP = "XX.XX.XX.XX";
+
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                IP = endPoint.Address.ToString();
+            }
+
+            label1.Text = "Waiting at " + IP + ", 4224";
+            label1.Visible = true;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void UpdateStatus(string status)
+        {
+            textBox1.Text += status + Environment.NewLine;
+        }
+
+        private void backgroundWorker1_DoWork(object sen, DoWorkEventArgs e)
+        {
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 4224);
+
+            UdpClient newsock = new UdpClient(ipep);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                if (backgroundWorker1.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    newsock.Close();
+                    return;
+                }
+
+                byte[] data = newsock.Receive(ref sender);
+
+                WriteTextBox(Encoding.ASCII.GetString(data));
+            }
+
+            void WriteTextBox(String message)
+            {
+                Invoke((MethodInvoker)(() =>
+                {
+                    textBox1.Text += message + Environment.NewLine;
+                }));
+            }
+        }
+    }
+}
